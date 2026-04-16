ee0518fda54b010846a8acc59800dd9a3f146816
Aleksey Shakhmut <leha.shakhmut@gmail.com>
2026-04-16 14:54:24 +0300

    Прееписан класс OpenAIClient

--- genai-api-tests/clients/openai.client.ts
+++ genai-api-tests/clients/openai.client.ts
@@ -1,7 +1,11 @@
 import { APIRequestContext } from '@playwright/test';
 
 export class OpenAIClient {
-    constructor(private request: APIRequestContext) {}
+    request: APIRequestContext;
+
+    constructor(request: APIRequestContext) {
+        this.request = request;
+    }
 
     async createResponse(input: string, options: any = {}) {
         return await this.request.post('responses', {
