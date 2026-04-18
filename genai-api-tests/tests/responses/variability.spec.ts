b20d26fcf37360f81a94733f9778ca4124585c47
Aleksey Shakhmut <leha.shakhmut@gmail.com>
2026-04-18 19:57:54 +0300

    Изменены названия констант в тесте на вариативность

--- genai-api-tests/tests/responses/variability.spec.ts
+++ genai-api-tests/tests/responses/variability.spec.ts
@@ -4,15 +4,15 @@ import { OpenAIClient } from '../../clients/openai.client';
 test('Проверяем, что модель даёт разные ответы при повышенной температуре', async ({ apiAuth }) => {
     const client = new OpenAIClient(apiAuth);
 
-    const r1 = await client.createResponse('Tell a joke', { temperature: 1 });
-    const r2 = await client.createResponse('Tell a joke', { temperature: 1 });
+    const response1 = await client.createResponse('Tell a joke', { temperature: 1 });
+    const response2 = await client.createResponse('Tell a joke', { temperature: 1 });
 
-    const b1 = await r1.json();
-    const b2 = await r2.json();
+    const body1 = await response1.json();
+    const body2 = await response2.json();
 
-    const t1 = b1.output[0].content[0].text;
-    const t2 = b2.output[0].content[0].text;
+    const joke1 = body1.output[0].content[0].text;
+    const joke2 = body2.output[0].content[0].text;
 
-    expect(t1).not.toEqual(t2);
+    expect(joke1).not.toEqual(joke2);
   
 });
